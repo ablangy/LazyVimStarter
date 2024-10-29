0b52b3687b6439c955c29b2e0e277e196b3212ee
Antoine BLANGY <antoine.blangy@csgroup.eu>
2024-10-29 13:31:58 +0100

    colorscheme: Add 'dracula' theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+  -- add dracula
+  { "Mofiqul/dracula.nvim" },
+
+  -- Configure LazyVim to load dracula
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
+
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+-- 
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   }
+-- }
